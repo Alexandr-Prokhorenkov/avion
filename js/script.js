cf7bad9944406f588b75ecc920c71c275c5817b5
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2023-12-10 21:00:49 +0300

    добавил счетчик лайков

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let heart = document.querySelector('.heart');
+let heart2 = document.querySelector('.heart2');
+let likesNumer = document.querySelector('.likes-number');
+let likesNumer2 = document.querySelector('.likes-number2');
+
+heart.onclick = function () {
+  if (heart.classList.contains('added')) {
+    likesNumer.textContent--;} else {likesNumer.textContent++;}
+  heart.classList.toggle('added');
+}
+
+heart2.onclick = function () {
+  if (heart2.classList.contains('added')) {
+    likesNumer2.textContent--;} else {likesNumer2.textContent++;}
+  heart2.classList.toggle('added');
+}
